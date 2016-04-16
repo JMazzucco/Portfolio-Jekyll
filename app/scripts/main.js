2d3a76e35a64c5485fd52f92f72450189e73139a
John Mazzucco <John.Mazzucco@gmail.com>
2016-04-16 15:08:30 -0400

    Recreate Yeoman scaffolding using Backbone generator. Install Neat, Bourbon and Grunt-Contrib-Sass

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,18 @@
-console.log('\'Allo \'Allo!');
+/*global portfolio, $*/
+
+
+window.portfolio = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  init: function () {
+    'use strict';
+    console.log('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function () {
+  'use strict';
+  portfolio.init();
+});
